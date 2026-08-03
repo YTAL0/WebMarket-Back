672de355e6707221d8a4f1a12a72219db49e7ec4
YTAL0 <ytalo122@hotmail.com>
2026-08-03 20:22:04 -0300

    Sec

--- src/main/java/org/webmarketapp/webmarket/WebMarketApplication.java
+++ src/main/java/org/webmarketapp/webmarket/WebMarketApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebMarketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebMarketApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebMarketApplication.class, args);
+    }
 
 }
